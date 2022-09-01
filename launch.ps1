acc95c302cd16c9eb201e4e164d9dfbba77f0563
0x8100 <60416837+0x8100@users.noreply.github.com>
2022-09-01 16:41:25 +0900

    Some modification for me

--- launch.ps1
+++ launch.ps1
@@ -3,21 +3,22 @@
 # and modified for Ubuntu focal.
 
 # Download from http://cloud-images.ubuntu.com/releases/focal/release/
-$imgPath = "${Env:USERPROFILE}\Downloads\ubuntu-20.04-server-cloudimg-amd64.img"
+$iso = "${Env:Home}\Hyper-V\vm-iso\ubuntu-20.04-server-cloudimg-amd64.img"
+$qemu = "${Env:Home}\Hyper-V\qemu\qemu-img"
 
 $VMName = "primary"
 $virtualSwitchName = "WinNAT"
-$vmPath = "${Env:Public}\Documents\Hyper-V\$VMName"
+$vmPath = "${Env:Home}\Hyper-V\$VMName"
 
-$vhdx = "$($vmPath)\test.vhdx"
+$vhdx = "$($vmPath)\primary.vhdx"
 $cloudInitIso = "$($vmPath)\metadata.iso"
 
 New-Item "$vmPath" -type directory -Force | Out-Null
 & .\cloudinitiso.exe -iso $cloudInitIso
 
 # Download qemu-img from http://www.cloudbase.it/qemu-img-windows/
-# and extract it to C:\qemu-img
-& C:\qemu-img\qemu-img convert -f qcow2 $imgPath -O vhdx -o subformat=dynamic $vhdx
+# and extract it to ${Env:Home}\Hyper-V\qemu
+& $qemu convert -f qcow2 $iso -O vhdx -o subformat=dynamic $vhdx
 Resize-VHD -Path $vhdx -SizeBytes 100GB
 
 # Create new virtual machine and start it
